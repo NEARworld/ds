7578acd999de9905942d3bac9f402cdb8d45fce9
nearworld <artemismars2@outlook.com>
2023-11-11 12:21:34 +0900

    feat: all data structure into one node project

---
+++ index.js
@@ -0,0 +1,13 @@
+import { Queue } from './queue/queue.js';
+
+const queue = new Queue();
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.enqueue(40);
+queue.enqueue(50);
+
+for (const data of queue.traverse()) {
+  console.log(data);
+}
--- linked_list/single_linked_list.js
+++ linked_list/single_linked_list.js
@@ -1,4 +1,4 @@
-class SingleLinkedList {
+export class SingleLinkedList {
   constructor() {
     this.head = null;
   }
@@ -13,7 +13,7 @@ class SingleLinkedList {
   }
 
   remove(value) {
-    if (!list.head) return "list is empty";
+    if (!list.head) return 'list is empty';
 
     let currentNode = list.head;
     let previousNode = null;
@@ -35,21 +35,9 @@ class SingleLinkedList {
   }
 }
 
-class Node {
+export class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
   }
-}
-
-const list = new SingleLinkedList();
-
-list.append(1);
-list.append(2);
-list.append(3);
-list.append(4);
-list.append(5);
-
-for (const node of list.traverse()) {
-  console.log(node);
 }
