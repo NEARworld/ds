e031b2249e87d89f26e66bda14da4ade5e53e3f0
nearworld <artemismars2@outlook.com>
2023-11-22 16:47:25 +0900

    feat: depth first traverse on binary tree using stack

---
+++ tree/binary_tree_with_stack.js
@@ -0,0 +1,26 @@
+import { BinaryTree } from './BinaryTree.js';
+import { BinaryTreeNode } from './BinaryTreeNode.js';
+
+const bt = new BinaryTree();
+bt.root = new BinaryTreeNode('a');
+const b = new BinaryTreeNode('b');
+const c = new BinaryTreeNode('c');
+const d = new BinaryTreeNode('d');
+const e = new BinaryTreeNode('e');
+
+bt.root.left = b;
+b.left = c;
+c.left = d;
+c.right = e;
+
+function depthFirstTraverse(root) {
+  const stack = [root];
+  while (stack.length > 0) {
+    const current = stack.pop();
+    console.log(current.value);
+    if (current.left) stack.push(current.left);
+    if (current.right) stack.push(current.right);
+  }
+}
+
+depthFirstTraverse(bt.root);
